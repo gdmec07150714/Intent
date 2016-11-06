7ed4d401cede70bb9bba507dfdc78c1127f28e64
07150714 <546470378@qq.com>
2016-11-06 13:22:34 +0800

    Initial commit

---
+++ app/src/main/java/s07150714/gdmec/com/intents/IntentDemo3.java
@@ -0,0 +1,17 @@
+package s07150714.gdmec.com.intents;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/10/27.
+ */
+public class IntentDemo3  extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_demo3);
+
+
+    }
+}
